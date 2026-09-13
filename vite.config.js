0860b3721a3d6089ffd913beb5fe9750bd232871
lokeshlikhar <lokeshlikhar01@gmail.com>
2026-09-13 20:15:39 +0530

    redeploy after fixing error

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,5 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          router: ["react-router-dom"],
-        },
-      },
-    },
   },
 });
